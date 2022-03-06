90c20ea4f2b299eca48e4e47ec0041e7aad93ff2
GANGASAGAR GOND <86286917+Gangasagar237@users.noreply.github.com>
2022-03-06 19:55:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function showimg(shomImg) {
+    var iPhone = document.getElementById("imagebox");
+    iPhone.src = shomImg.src;
+}
+
+
+function clickimg(smallImg){
+    var fullImg = document.getElementById("imagebox");
+    fullImg.src = smallImg.src;
+}
